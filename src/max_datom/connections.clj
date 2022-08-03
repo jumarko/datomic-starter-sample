f62d8c22d73e24026d86a2bdd4b78f7942edb4d9
Juraj Martinka <jumarko@gmail.com>
2022-08-03 23:06:14 +0200

    Max Datom - lessons 5 and 6 - pull syntax.
    Finally, I have some data to play with thanks to the pull syntax,
    which makes them easy to 'pull'.

--- src/max_datom/connections.clj
+++ src/max_datom/connections.clj
@@ -13,12 +13,30 @@
 
   (d/create-database db-uri)
 
+  ;; delete the db if you want to start from scratch
+  ;; - you have to re-connect in that case
+  #_(d/delete-database db-uri)
+
+
   ;; transact schema
   @(d/transact @conn (edn/read-string (slurp "src/max_datom/schema.edn")))
 
   ;; add seed data
   @(d/transact @conn (edn/read-string (slurp "src/max_datom/data.edn")))
 
+
+  .)
+
+;; list all the books and authors in the db to include them in data.edn
+;; see level 5: https://max-datom.com/#/FE78C28B-3759-4AA1-94A0-CC8057D2255A
+(comment
+  (d/q '[:find (pull ?e [*])
+         :where [?e :book/author]]
+       (db))
+
+  (d/q '[:find (pull ?e [*])
+         :where [?e :author/first-name]]
+       (db))
 
   .)
 
---
+++ src/max_datom/level_4.clj
@@ -0,0 +1,20 @@
+(ns maxdatom.level-4
+  "Unification.
+  See https://max-datom.com/#/4C495D7B-C678-4057-9EB4-1BD8F57A15AC"
+  (:require
+   [max-datom.connections :refer [db]]
+   [datomic.api :as d]))
+
+(comment
+
+  ;; return entity id too
+  (d/q '[:find ?pub-date
+         ;; notice we must use ?e (or similar),
+         ;; cannot use _ !!! (because it would return all the books' publication dates)
+         :where [?e :book/publication-date ?pub-date]
+                [?e :book/name "Process and Grow RAM"]]
+       (db))
+  ;; should return:
+  ;;=> [[#inst "2032-07-04T00:00:00.000-00:00"]]
+
+  .)
